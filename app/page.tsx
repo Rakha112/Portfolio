fb2980a4bb06e2990ed7c0261b9e1b4041e8963b
Rakha112 <rakhawibowo1998@gmail.com>
2023-11-18 15:55:34 +0700

    Feat: Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import styles from "./page.module.scss";
 import HiddenNav from "./components/NavBar/HiddenNav";
 import BackDropNav from "./components/NavBar/BackDropNav";
 import NavBar from "./components/NavBar/NavBar";
-
+import Me from "./sections/Me/Me";
+import About from "./sections/About/About";
+import Accordion from "./sections/Accordion/Accordion";
 type Props = {};
 
 const page = (props: Props) => {
@@ -12,6 +14,9 @@ const page = (props: Props) => {
       <HiddenNav />
       <BackDropNav />
       <NavBar />
+      <Me />
+      <About />
+      <Accordion />
     </main>
   );
 };
