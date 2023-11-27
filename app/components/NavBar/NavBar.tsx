c193020231f1e4c0e4f9a507d8713de49789dc7d
Rakha112 <rakhawibowo1998@gmail.com>
2023-11-27 10:21:45 +0700

    Feat: Hide Navbar when scroll down

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./navBar.module.scss";
 import Magnetic from "../Magnetic/Magnetic";
 import Ham from "./Ham";
@@ -10,20 +10,46 @@ import {
   useScroll,
   useTransform,
 } from "framer-motion";
+import { useLenis } from "../LenisScroller/LenisScroller";
+import Lenis from "@studio-freight/lenis";
 
 const NavBar = () => {
   const { scrollYProgress } = useScroll();
   const blurValue = useTransform(scrollYProgress, [0, 0.01], [0, 12]);
   const blur = useMotionTemplate`blur(${blurValue}px)`;
+  const lenis = useLenis();
+  const [direction, setDirection] = useState(0);
+  console.log(direction);
+
+  const prevDirectionRef = useRef<null | number>(null);
+  const handleScroll = useCallback(({ direction }: { direction: number }) => {
+    if (prevDirectionRef.current !== direction) {
+      setDirection(direction);
+      prevDirectionRef.current = direction;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (lenis) {
+      lenis.on("scroll", handleScroll);
+    }
+
+    // Cleanup function to remove the event listener
+    return () => {
+      if (lenis) {
+        lenis.off("scroll", handleScroll);
+      }
+    };
+  }, [lenis, handleScroll]);
 
   return (
     <motion.nav
       className={styles.container}
       style={{ backdropFilter: blur, WebkitBackdropFilter: blur }}
       initial={{ y: -300 }}
-      animate={{ y: 0 }}
+      animate={{ y: direction === 0 || direction === -1 ? 0 : -200 }}
       transition={{
-        delay: 3.5,
+        delay: direction === 0 ? 3.5 : 0,
         duration: 1,
         ease: [0.22, 1, 0.36, 1],
       }}
