90cede1c78d0f0c1997ec4970d26e36896dd5399
Rakha112 <rakhawibowo1998@gmail.com>
2023-12-03 19:52:51 +0700

    chore: add env in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  env: {
+    API_URL: process.env.API_URL,
+  },
 };
 
 module.exports = nextConfig;
