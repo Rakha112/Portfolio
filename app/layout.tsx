874bfe7a6567a6d49553989246257a6edbe2e700
Rakha112 <rakhawibowo1998@gmail.com>
2025-04-14 17:27:55 +0700

    feat: integrate ReactLenis for smooth scrolling in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ReactLenis from "lenis/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReactLenis root>{children}</ReactLenis>
       </body>
     </html>
   );
