3138f937fe6a94bb4dfade766ce233cb35aa345e
Rakha112 <rakhawibowo1998@gmail.com>
2025-04-14 17:28:45 +0700

    feat: add About section with animated text and profile image

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import Cursor from "@/components/Cursor";
 import HiddenNav from "@/components/NavBar/HiddenNav";
 import NavBar from "@/components/NavBar/NavBar";
 import Welcome from "@/components/Welcome";
+import About from "@/sections/About";
 import Hero from "@/sections/Hero";
 
 const page = () => {
   return (
-    <main className="flex h-screen w-screen overflow-hidden font-geist">
-      <Cursor />
-      <NavBar />
-      <HiddenNav />
-      <Welcome />
-      <Hero />
+    <main className="relative flex max-w-[100vw] flex-col items-center justify-center bg-warnaHitam">
+      <div className="flex max-w-[1920px] flex-col items-center justify-center">
+        <Cursor />
+        <NavBar />
+        <HiddenNav />
+        <Welcome />
+        <Hero />
+        <About />
+      </div>
     </main>
   );
 };
